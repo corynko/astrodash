2f12a60feafb11bc7173d06e0fe8952ac2692258
corynko <ethankotel@gmail.com>
2023-08-18 18:10:22 -0600

    began buildout of home page, added react-sparkle as an experiment

--- src/components/navComponents/Nav.js
+++ src/components/navComponents/Nav.js
@@ -1,5 +1,26 @@
-import React from "react";
+// non-animated imports
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import AppBar from "@mui/material/AppBar";
+
+import LogoDrawDesktop from "./desktop/logoDrawDesktop";
 
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <AppBar position="static" className="appBar">
+      <Toolbar className="toolBar" disableGutters>
+        <Box
+          sx={{
+            width: 1,
+            display: "flex",
+            flexGrow: 1,
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <LogoDrawDesktop />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
+import Sparkles from "react-sparkle";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex column center relative coverPage">
+      <Sparkles
+        count={15}
+        minSize={2}
+        maxSize={8}
+        overflowPx={0}
+        fadeOutSpeed={4}
+        flicker={true}
+        flickerSpeed={"slower"}
+      />
+      Home
+    </div>
+  );
 }
