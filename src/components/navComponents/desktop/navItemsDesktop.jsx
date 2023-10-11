98af8d414a3d3b129ebc331f8c534379a79ffda3
corynko <ethankotel@gmail.com>
2023-10-11 15:18:38 -0600

    implemented modals for signup and signin, added form as iframe embed pending full styling for native react format instead

--- src/components/navComponents/desktop/navItemsDesktop.jsx
+++ src/components/navComponents/desktop/navItemsDesktop.jsx
@@ -3,6 +3,9 @@ import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+import SignupModal from "../../reactFormComponents/signupModal";
+import SigninModal from "../../reactFormComponents/signinModal";
+
 function NavItemsDesktop() {
   let divVariants = {
     initial: { opacity: 0 },
@@ -26,38 +29,10 @@ function NavItemsDesktop() {
         }}
       >
         <Link className="m10" to="/signup">
-          <Button
-            className="navLink"
-            sx={{
-              marginX: "1rem",
-              fontWeight: 300,
-              fontSize: "115%",
-              fontFamily: "GothamLight",
-              whiteSpace: "nowrap",
-              flexGrow: 1,
-              color: "#f5f5f5",
-            }}
-            fullWidth={true}
-          >
-            sign up
-          </Button>
+          <SignupModal />
         </Link>
         <Link className="m10" to="/login">
-          <Button
-            className="navLink"
-            sx={{
-              marginX: "1rem",
-              fontWeight: 300,
-              fontSize: "115%",
-              fontFamily: "GothamLight",
-              whiteSpace: "nowrap",
-              flexGrow: 1,
-              color: "#f5f5f5",
-            }}
-            fullWidth={true}
-          >
-            log in
-          </Button>
+          <SigninModal />
         </Link>
         {/* TODO: only show profile link if logged in */}
         <Link className="m10" to="/profile">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import { useTheme } from "@mui/material/styles";
 import Sparkles from "react-sparkle";
 import { motion } from "framer-motion";
 
+import SignupForm from "../components/reactFormComponents/signupForm";
+
 export default function Home() {
   let divVariants = {
     start: { opacity: 0 },
@@ -20,13 +22,13 @@ export default function Home() {
   return (
     <div className="flex column center relative coverPage homeCoverImg">
       <Sparkles
-        count={15}
+        count={18}
         minSize={2}
         maxSize={8}
         overflowPx={0}
         fadeOutSpeed={4}
         flicker={true}
-        flickerSpeed={"slower"}
+        flickerSpeed={"slowest"}
       />
       <motion.div
         className="textCenter"
@@ -34,9 +36,11 @@ export default function Home() {
         initial="start"
         animate="finished"
       >
-        <h1 className="homeHeader">welcome to astroDash</h1>
-        <h3 className="homeHeader">astroDash makes it easy to plan a shoot</h3>
-        <h4 className="homeHeader">
+        <h1 className="homeHeader m25 p75">welcome to astroDash</h1>
+        <h3 className="homeHeader m25">
+          astroDash makes it easy to plan a shoot
+        </h3>
+        <h4 className="homeHeader m25">
           aggregate forecast data, AR landscape images, and more in one place
         </h4>
       </motion.div>
