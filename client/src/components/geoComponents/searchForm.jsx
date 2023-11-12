95f13994a1be65cc34b434d33f12bcac074e7d00
corynko <ethankotel@gmail.com>
2023-11-12 14:21:12 -0700

    integrating geoapify API to translate location searches into coordinates. Preparing to chain weather API calls off coordinates. Devs will need a .env in their client folder, reach out to corynko for API key

--- client/src/components/geoComponents/searchForm.jsx
+++ client/src/components/geoComponents/searchForm.jsx
@@ -2,31 +2,42 @@ import { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import axios from "axios";
 
 function SearchForm() {
-  const SearchBar = ({ setSearchQuery }) => (
-    <form>
+  const [location, setLocation] = useState("");
+
+  const handleLocationSearch = async (event) => {
+    event.preventDefault();
+    const encodedLocation = encodeURIComponent(location);
+    const apiKey = process.env.REACT_APP_GEOAPIFY_API_KEY;
+    const url = `https://api.geoapify.com/v1/geocode/search?text=${encodedLocation}&apiKey=${apiKey}`;
+    console.log(url); // This should now output the correct URL
+
+    try {
+      const response = await axios.get(url);
+      const coords = response.data.features[0].geometry.coordinates;
+      console.log(coords); // Do something with the coordinates
+    } catch (error) {
+      console.error("Error fetching geocode data:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLocationSearch}>
       <TextField
         id="search-bar"
         className="text"
-        onInput={(e) => {
-          setSearchQuery(e.target.value);
-        }}
-        label="Enter a city name"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+        label="Search for a Location"
         variant="outlined"
-        placeholder="Search..."
         size="small"
       />
       <IconButton type="submit" aria-label="search" size="large">
-        <SearchIcon style={{ fill: "blue" }} />
+        <SearchIcon style={{ fill: "#5f75bf" }} />
       </IconButton>
     </form>
-  );
-  return (
-    <>
-      <SearchBar />
-    </>
   );
 }
 
