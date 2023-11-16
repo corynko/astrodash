a6966881fd46e05678e90d6df2b3421ca3d2c39f
Andrick Siegmund <andrick.siegmund@gmail.com>
2023-11-16 16:32:39 -0700

    eliminate extraneous code in server/app.js. Eliminate all session node modules. Installed JWT. Update User Model to reflect information collected for DB. Create middleware auth functions in Utils.

--- server/controller/api/users.js
+++ server/controller/api/users.js
@@ -1,9 +1,60 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+// const verifyToken = require('../../utils/auth');
+// const authenticateToken = require('../../utils/auth');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/signup', async (req, res) => {
+  try {
+    console.log("Enetered try block");
+    console.log(req.body);
+    const userData = await User.create(
+      {
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email,
+        password: req.body.password,
+      }
+    );
+    res.status(201).json(userData);
+  } catch(err) {
+    res.status(400).json(err); 
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userData = await User.findOne({ where: { email: email } });
+
+    if (!userData) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // Respond with user data or a success message
+    res.status(200).json(userData);
+  } catch (err) {
+    console.error('Error during login', err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- server/models/User.js
+++ server/models/User.js
@@ -18,6 +18,18 @@ User.init(
             allowNull: false,
             primaryKey: true,
         },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true,
+            validate: {
+                is: /^\d{10,}$/i, // regex for 10 digits
+            }
+        },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -37,14 +49,6 @@ User.init(
                     }
                 }
             }
-        },
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: true, 
         },
     },
     {
